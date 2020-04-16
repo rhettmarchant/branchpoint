65bc4ac8aacc69a75bbeb32dcdc98f12b8d2c049
Rhett Marchant <rhett.marchant@gmail.com>
2020-04-16 16:02:57 +1000

    added Seq Collection.R

---
+++ BPSZ Seq Collection.R
@@ -0,0 +1,18 @@
+#Collecting BPSZ sequences
+
+call <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",bfa2$exonStarts[3],";end=",bfa2$exonEnds[3]+2,sep="")
+call
+get_Seq <- fromJSON(call)
+unlist(get_Seq)[7]
+
+seqbfa <- head(bfa2,10)
+seqbfa
+
+ben <- 0
+
+for(i in 1:10){
+  caller <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",seqbfa$BPSZ[i],";end=",seqbfa$exonEnds[i],sep="")
+  seQ <- fromJSON(call)
+  ben[i] = unlist(seQ)[7]
+}
+
