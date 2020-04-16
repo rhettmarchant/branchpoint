1fb964bedc1e5e769684623b42c532000f52c628
Rhett Marchant <rhett.marchant@gmail.com>
2020-04-16 16:03:31 +1000

    modified Source.R

--- Source.R
+++ Source.R
@@ -10,7 +10,6 @@ geneframe <- tbl_df(as.data.frame(get_Genes))
 gexons <- geneframe %>% select(knownGene.name, knownGene.exonCount, knownGene.exonStarts, knownGene.exonEnds)
 
 
-
 bfa2 <- data.frame(name=rep(bfa$knownGene.name, bfa$knownGene.exonCount), exonStarts = unlist(strsplit(bfa$knownGene.exonStarts, ",")), exonEnds = unlist(strsplit(bfa$knownGene.exonEnds,",")))
 
 temp=0
@@ -19,5 +18,6 @@ for(i in 1:length(unique(bfa2$name))){
   temp[i] = list(seq(from=1, to=bfa$knownGene.exonCount[i]))
 }
 
-bfa2 <- bfa2 %>% mutate(exonNum = unlist(temp)) %>% select(name, exonNum, exonStarts, exonEnds)
+bfa2 <- bfa2 %>% mutate(exonNum = unlist(temp),exonStarts = as.numeric(exonStarts), exonEnds = as.numeric(exonEnds), BPSZ = exonStarts-100) %>% select(name,exonNum,BPSZ,exonStarts,exonEnds)
+
 head(bfa2, 50)
