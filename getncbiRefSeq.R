bfcdbc0fe9467e7cebbf2e6212f76699f8a7e3b3
Rhett Marchant <rhett.marchant@gmail.com>
2020-04-19 12:03:32 +1000

    cleaned code and created new dataframe with unique exons only

---
+++ getncbiRefSeq.R
@@ -0,0 +1,47 @@
+#Collecting genes and converting into individual exon coordinates
+library(jsonlite)
+library(tidyverse)
+
+#Collect ncbiRefSeq table for chr12 from UCSC API
+#THIS DOES NOT GET ALL GENES FOR chr12 ONLY 750000 to 55700000
+genecall <- paste("http://api.genome.ucsc.edu/getData/track?genome=hg38;track=ncbiRefSeq;chrom=chr12;start=750000;end=55700000")
+genecall_JSON <- fromJSON(genecall)
+
+#Convert API JSON to data.frame containing only ncbiRefSeq data
+RefSeqdf <- data.frame(genecall_JSON$ncbiRefSeq)
+
+#Create attributes to list each exon independently
+#Assign a number to each exon based on the exon count and its position in the gene
+exnCount=0
+for(i in 1:length(unique(RefSeqdf$name))){exnCount[i] = list(seq(from=1, to=RefSeqdf$exonCount[i]))}
+
+refs = rep(RefSeqdf$name, RefSeqdf$exonCount)
+genenames = rep(RefSeqdf$name2, RefSeqdf$exonCount)
+strands = rep(RefSeqdf$strand, RefSeqdf$exonCount)
+exonNums = unlist(exnCount)
+EXONStarts = as.numeric(unlist(strsplit(RefSeqdf$exonStarts, ",")))
+EXONEnds = as.numeric(unlist(strsplit(RefSeqdf$exonEnds,",")))
+BPSZs = EXONStarts - 100
+
+#Extract dataframe from RefSeqdf containing reference number (ref), gene name(name), strand direction(strand)
+#coordinates of exon start (exonStart), coordinates of exon end (exonEnd) and the branch point search zone 100nt downstream of exonStart (BPSZ)
+
+allisoformExons <- data.frame(ref=refs,name=genenames,strand=strands,exonNum=exonNums,exonStarts = EXONStarts,exonEnds = EXONEnds,BPSZ=BPSZs)
+
+#Create dataframes of forward and reverse exons for troubleshooting/development
+
+fwdexons <- allisoformExons %>% filter(strand == "+")
+rvsexons <-  allisoformExons %>% filter(strand == "-")
+
+#Remove all duplicate exons i.e. multiple isoforms
+collector <- 0
+newExons <- 0
+for(i in seq_along(allisoformExons$name)){
+    if(!(paste(allisoformExons$name[i],allisoformExons$exonNum[i],sep="_") %in% collector)){
+        collector[i] <- paste(allisoformExons$name[i],allisoformExons$exonNum[i],sep="_")
+        newExons[i] <- i
+    }
+}
+
+#Create new data frame containing each gene exon only once
+alluniqueExons <- allisoformExons[newExons,] %>% filter(!is.na(name))
