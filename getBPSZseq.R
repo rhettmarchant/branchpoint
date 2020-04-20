baa9464ee08da5f5c6addfce757878998de0d649
rhettmarchant <rhett.marchant@gmail.com>
2020-04-20 18:05:33 +1000

    created a new function to get sequences for a list of genes and exons

--- getBPSZseq.R
+++ getBPSZseq.R
@@ -3,16 +3,30 @@
 #Still poorly tested with reverse strands
 #THIS IS WORKING NOWWW!!!!!!! But takes approx 1s/seq
 #DO NOT USE WITH MORE THAN 60 seqs at a time
-ben <- 0
-cls <- 0
-system.time(
-    for(i in 1:10){
-        caller <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",alluniqueExons$BPSZ[i],";end=",alluniqueExons$exonStarts[i], sep="")
-        seQ <- fromJSON(caller)
-        ben[i] = unlist(seQ)[7]
-        cls[i] <- caller
-    }
-)
 
-ben
-cls
+get_Seqs <- function(x){
+    #create a dataframe for the results
+    sequences <- data.frame(name=c(""),exon=c(""), sequence= c(""), stringsAsFactors = F)
+    #processes the gene name and exon number
+    requests <- strsplit(x,"#")
+    #creates an empty list for the sequences
+    APIseq <- 0
+    for(i in seq_along(x)){
+        #collects the gene name and exon number
+        request <- c(requests[[i]][1], requests[[i]][2])
+        #check that the gene name is valid and within alluniqueExons
+        if(request[1] %in% alluniqueExons$name){
+            #selects the BPSZ for the request
+            APIrequest <- unlist(alluniqueExons %>% filter(name== request[1] & exonNum==request[2]) %>% select(BPSZ, exonStarts))
+            #makes the call to the API
+            APIcall <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",APIrequest[1],";end=",APIrequest[2], sep="")
+            APIresult <- fromJSON(APIcall)
+            APIseq[i] <- unlist(APIresult)[7]
+        }else{
+           APIseq[i] <- paste(request[1],"INVALID CALL",sep="") 
+        }
+        #adds the results of the request ot the sequences dataframe
+        sequences[i,] <- c(request[1],request[2],APIseq[i])
+    }
+sequences
+}
