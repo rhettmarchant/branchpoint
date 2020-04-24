79517649901244b562d01060490027ee55d5a917
rhettmarchant <rhett.marchant@gmail.com>
2020-04-24 12:24:54 +1000

    now returns sequences from reverse strands. Added reversecomplement function and enabled returned sequence to be in the same orientation as forward strands so AGEZ_algo did not have to be edited.

--- getBPSZseq.R
+++ getBPSZseq.R
@@ -1,12 +1,22 @@
-#Gathers the last 100 nt of each intron
-#Do not use with exon 1 (for obvious reasons)
-#Still poorly tested with reverse strands
-#THIS IS WORKING NOWWW!!!!!!! But takes approx 1s/seq
-#DO NOT USE WITH MORE THAN 60 seqs at a time
+#Now collects the correct BPSZ for reverse strand exons
+
+
+revComplement <- function(x){
+    seqList <- unlist(str_split(toupper(x),""))
+    bases <- c("A","G","C","T")
+    names(bases) <- c("T","C","G","A")
+    bases
+    revSeq <- 0
+    for(i in seq_along(seqList)){
+        revSeq[i] <- bases[seqList[i]]
+        
+    }
+    paste(revSeq, sep="", collapse= "")
+}
 
 get_Seqs <- function(x){
     #create a dataframe for the results
-    sequences <- data.frame(name=c(""),exon=c(""), sequence= c(""), stringsAsFactors = F)
+    sequences <- data.frame(name=c(""),exon=c(""), strand=c(""), sequence= c(""), stringsAsFactors = F)
     #processes the gene name and exon number
     requests <- strsplit(x,"#")
     #creates an empty list for the sequences
@@ -17,22 +27,32 @@ get_Seqs <- function(x){
         #check that the gene name is valid and within alluniqueExons
         if(request[1] %in% alluniqueExons$name){
             #selects the BPSZ for the request
-            APIrequest <- unlist(alluniqueExons %>% filter(name== request[1] & exonNum==request[2]) %>% select(BPSZ, exonStarts))
+            APIrequest <- unlist(alluniqueExons %>% filter(name== request[1] & exonNum==request[2]) %>% select(BPSZ, exonStarts, exonEnds, strand))
             #makes the call to the API
-            APIcall <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",APIrequest[1],";end=",APIrequest[2], sep="")
-            APIresult <- fromJSON(APIcall)
-            APIseq[i] <- unlist(APIresult)[7]
+            if(APIrequest[4] == 2){
+                APIcall <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",APIrequest[1],";end=",APIrequest[2], sep="")
+                APIresult <- fromJSON(APIcall)
+                APIseq[i] <- unlist(APIresult)[7]
+            }
+            if(APIrequest[4] == 1){
+                APIcall <- paste("https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr12;start=",APIrequest[3],";end=",APIrequest[1], sep="")
+                APIresult <- fromJSON(APIcall)
+                APIseq[i] <- revComplement(stri_reverse(unlist(APIresult)[7]))
+            }
+            
+            
         }else{
-           APIseq[i] <- paste(request[1],"INVALID CALL",sep="") 
+            APIseq[i] <- paste(request[1],"INVALID CALL",sep="") 
         }
         #adds the results of the request ot the sequences dataframe
-        sequences[i,] <- c(request[1],request[2],APIseq[i])
+        sequences[i,] <- c(request[1],request[2],APIrequest[4],APIseq[i])
     }
-sequences
+    sequences
 }
 
-set.seed(2)
-test <- sample(alluniqueExons$exonname, 300, replace = F)
+set.seed(3)
+test <- sample(alluniqueExons$exonname, 100, replace = F)
 system.time(testSeqs <- get_Seqs(test))
 browseURL('https://www.youtube.com/watch?v=WhPvJOnHotE')
 AGEZ(testSeqs)
+
