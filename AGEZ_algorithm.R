7d2f5b60965a2f285822898045c636d174b05dbb
rhettmarchant <rhett.marchant@gmail.com>
2020-04-24 12:23:31 +1000

    fixed error by allowing termination if end of sequence reached. Works for all sequences now.

--- AGEZ_algorithm.R
+++ AGEZ_algorithm.R
@@ -3,37 +3,52 @@
 #Error in if (Seq[i] %in% c("g", "G") & Seq[i - 1] %in% c("a", "A") & i <  : argument is of length zero
 
 AGEZ <- function(y){
-    y=testSeqs
-    AGEZdf <- data.frame(name=y$name,AG3SS=rep("0",nrow(y)),length=rep(0,nrow(y)),sequence=rep("0",nrow(y)), stringsAsFactors = FALSE)
+    AGEZdf <- data.frame(name=y$name,term=rep("0",nrow(y)),AG3SS=rep("0",nrow(y)),length=rep(0,nrow(y)),sequence=rep("0",nrow(y)), stringsAsFactors = FALSE)
     g <- y$sequence
     for(x in seq_along(g)){
         SeqL <- str_length(g[x])
         Seq <- strsplit(g[x],"")[[1]]
         #print(c(Seq[1:10], SeqL))
         errors <- 0
+        counter <- 0
     
         if(Seq[SeqL] %in% c("g","G") & Seq[SeqL-1] %in% c("a","A")){
-        
+            AGEZdf$term[x] = "AG"
             for(i in seq(SeqL,1,-1)){
-                if(i < SeqL & Seq[i] %in% c("g","G") & Seq[i-1] %in% c("a","A")){
-                    if(i>13){
-                        m <- i-13
-                    }
-                    AGEZSeq <- Seq[seq(m,SeqL,1)]
+                if(i==1){
+                    AGEZSeq <- Seq[seq(1,SeqL,1)]
                     AGEZdf$sequence[x] <- paste(AGEZSeq, sep="", collapse="")
                     AGEZdf$length[x] <- str_length(AGEZdf$sequence[x])
                     AGEZdf$AG3SS[x] <- TRUE
                     break
-                }else if(i==1){
-                    break
-                }    
+                }  else if(Seq[i] %in% c("g","G") & Seq[i-1] %in% c("a","A")){
+                    counter <- counter + 1
+                    if(counter == 2 & i>12){
+                        AGEZSeq <- Seq[seq((i-12),SeqL,1)]
+                        AGEZdf$sequence[x] <- paste(AGEZSeq, sep="", collapse="")
+                        AGEZdf$length[x] <- str_length(AGEZdf$sequence[x])
+                        AGEZdf$AG3SS[x] <- TRUE
+                        break
+                    }
+                    else if(i == 1){
+                        AGEZSeq <- Seq[seq(1,SeqL,1)]
+                        AGEZdf$sequence[x] <- paste(AGEZSeq, sep="", collapse="")
+                        AGEZdf$length[x] <- str_length(AGEZdf$sequence[x])
+                        AGEZdf$AG3SS[x] <- TRUE
+                        break
+                    }
+
+                } 
         
             }
         }else{
             AGEZdf$AG3SS[x] <- FALSE
-            AGEZdf$sequence[x] <- "ERROR INVALID 3'SS: No AG"
-            AGEZdf$length[x] <- NA
+            AGEZdf$term[x] <- paste(Seq[(SeqL-1):SeqL],sep="",collapse="")
+            AGEZSeq <- Seq[seq(1,SeqL,1)]
+            AGEZdf$sequence[x] <- paste(AGEZSeq, sep="", collapse="")
+            AGEZdf$length[x] <- str_length(AGEZdf$sequence[x])
         }
     }
     tbl_df(AGEZdf %>% filter(AG3SS=="TRUE"))
+    
 }
